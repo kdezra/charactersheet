3aca038d0da06e9a7cf543e3c38088869932b8a9
kdezra <korrn@proton.me>
2025-07-04 18:01:04 -0500

    mouse event check

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const fileInput = document.getElementById('portrait-input');
 const image = document.getElementById('portrait');
 
+const ratings = {}
+
 document.addEventListener("DOMContentLoaded", (event) => {
     fileInput.addEventListener('change', (e) => {
         const file = e.target.files[0];
@@ -8,17 +10,48 @@ document.addEventListener("DOMContentLoaded", (event) => {
         let fileReader = new FileReader();
         fileReader.readAsDataURL(file);
         fileReader.onload = function () {
-            // images[0].setAttribute('src', fileReader.result);
             image.setAttribute('style', `background-image: url('${fileReader.result}')`);
         }
     });
 
-    document.querySelectorAll(".rating").forEach(el => {
-        el.addEventListener("mouseover", (e) => {
+
+    document.querySelectorAll(".rating").forEach((el) => {
+        const elLabel = el.lastElementChild.textContent;
+        ratings[`${elLabel}-rating`] = {
+            'element':el,
+            'stars': 0,
+            'loc': 0
+        };
+        el.addEventListener("mousemove", (e) => {
             const bounds = e.target.getBoundingClientRect();
-            const eloc = e.x - bounds.x;
-            console.log(eloc);
+            ratings.loc = e.x - bounds.x;
         });
+        el.addEventListener("click", (e) => {
+            const elLabel = el.lastElementChild.textContent;
+            const elem = ratings[`${elLabel}-rating`]
+            const eloc = ratings.loc
+            if (eloc < 6) {
+                console.log(".5 star")
+            } else if (eloc < 15) {
+                console.log("1 star")
+            } else if (eloc < 28) {
+                console.log("1.5 star")
+            } else if (eloc < 37) {
+                console.log("2 star")
+            } else if (eloc < 46) {
+                console.log("2.5 star")
+            } else if (eloc < 55) {
+                console.log("3 star")
+            } else if (eloc < 65) {
+                console.log("3.5 star")
+            } else if (eloc < 76) {
+                console.log("4 star")
+            } else if (eloc < 89) {
+                console.log("4.5 star")
+            } else {
+                console.log("5 star")
+            }
+        })
     });
 })
 
