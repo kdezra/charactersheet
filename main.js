10cc102eaac940e8f7352958edce68ff892899d2
kdezra <korrn@proton.me>
2025-07-04 15:41:45 -0500

    script fix

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const fileInput = document.getElementById('portrait-input');
 const image = document.getElementById('portrait');
 
-fileInput.addEventListener('change', (e) => {
-    const file = e.target.files[0];
+document.addEventListener("DOMContentLoaded", (event) => {
+    fileInput.addEventListener('change', (e) => {
+        const file = e.target.files[0];
 
-    let fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload = function () {
-        // images[0].setAttribute('src', fileReader.result);
-        image.setAttribute('style', `background-image: url('${fileReader.result}')`);
-    }
+        let fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = function () {
+            // images[0].setAttribute('src', fileReader.result);
+            image.setAttribute('style', `background-image: url('${fileReader.result}')`);
+        }
+    })
 })
 
 $(".sheet-cont h2").on("click", function () {
